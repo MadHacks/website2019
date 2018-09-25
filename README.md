787ffb72045f2cf44d72c0d5d84e68a4ad41ff71
John Moss <johnmoss96@gmail.com>
2018-09-24 22:43:50 -0500

    24 sept

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # website2019
 Website for MadHacks 2019
+
+## TODO
+
+### Window manager framework
+* ~~Fix bug where you can move windows off the screen (Moss)~~
+* Add desktop icons behaviour (Moss)
+* Add title bar (Moss)
+* Fix currently-dragging window z-index issue (Moss)
+* Fix weird bug where window title gets dragged off screen, but everything else stays put (Moss)
+
+### Content
+* Add rest of the windows, barebones content (Stamper)
+
+### Style
+* Theme the css according to the style guide (Stamper)
+* Find body font (Stamper)
+* Find appropriate desktop icons (Stamper)
--- controller.js
+++ controller.js
@@ -20,16 +20,35 @@ window.onload = function() {
 		relative_y = evt.data.sensorEvent.data.clientY - evt.data.sourceContainer.offsetTop;
 		frames_set[evt.data.sourceContainer]['init_mouse_start'] = [relative_x, relative_y];
 
-		console.log(relative_x, evt.data.sourceContainer.offsetWidth)
+		//console.log(relative_x, evt.data.sourceContainer.offsetWidth)
 		if (relative_x > evt.data.sourceContainer.offsetWidth - 20) {
 			evt.data.sourceContainer.style.display = "none";
 		}
+
+		evt.data.sourceContainer.style.zIndex = "1";
 	});
 	draggable.on('drag:move', (evt) => {
+		frame = evt.data.sourceContainer;
 		x = evt.data.sensorEvent.data.clientX;
 		y = evt.data.sensorEvent.data.clientY;
-		evt.data.sourceContainer.style.left = x - frames_set[evt.data.sourceContainer]['init_mouse_start'][0];
-		evt.data.sourceContainer.style.top = y - frames_set[evt.data.sourceContainer]['init_mouse_start'][1];
+
+		x_ = frames_set[frame]['init_mouse_start'][0];
+		y_ = frames_set[frame]['init_mouse_start'][1];
+
+		bound_x = x + frame.offsetWidth - x_;
+		bound_y = y + frame.offsetHeight - y_;
+
+		anchor_x = x - x_;
+		anchor_y = y - y_;
+
+		// Check against right bound
+		if (bound_x < window.innerWidth && anchor_x > 0 ) { frame.style.left = x - x_; }
+		else if (bound_x > window.innerWidth) { frame.style.left = window.innerWidth - frame.offsetWidth - 1; }
+		else if (anchor_x < 0) { frame.style.left = 1; }
+
+		if (bound_y < window.innerHeight && anchor_y > 0) { frame.style.top = y - y_; }
+		else if (bound_y > window.innerHeight) { frame.style.top = window.innerHeight - frame.offsetHeight - 1; }
+		else if (anchor_y < 0) { frame.style.top = 1; }
 	});
 	//draggable.on('drag:stop', () => console.log('drag:stop'));
 }
