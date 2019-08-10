351c88cfbcb08f195e581c611c7a1f3f7405cc23
Hayden Pilsner <haydenpilsner@gmail.com>
2019-08-10 14:11:21 -0500

    Add Leopold and Nelson quote

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import Grid from '@material-ui/core/Grid';
 import Header from './components/Header';
 import About from './components/About';
 import Sponsors from './components/Sponsors';
+import QuoteLeft from './components/QuoteLeft';
+import QuoteRight from './components/QuoteRight';
+
+import aldo from './img/aldo_leopold.jpg';
+import gaylord from './img/gaylord_nelson.jpg';
 
 const theme = createMuiTheme({
   typography: {
@@ -25,8 +30,27 @@ function App() {
           <About />
         </Grid>
         <Grid container item xs={12} spacing={0}>
+          <QuoteLeft
+            avatar={aldo}
+            author={
+              <span>Aldo Leopold, <em>A Sand County Almanac</em></span>
+            }
+            title={
+              <p>Conservationist & Professor Emeritus University of Wisconsin</p>
+            }
+            msg='“We abuse land because we regard it as a commodity belonging to us. When we see land as a community to which we belong, we may begin to use it with love and respect.”'/>
+        </Grid>
+        <Grid container item xs={12} spacing={0}>
+          <QuoteRight
+            avatar={gaylord}
+            author="Gaylord Nelson"
+            title="Former Wisconsin Senator & Founder of Earth Day"
+            msg="“The ultimate test of man's conscience may be his willingness to sacrifice something today for future generations whose words of thanks will not be heard.”" />
+        </Grid>
+        <Grid container item xs={12} spacing={0}>
           <Sponsors />
         </Grid>
+        
       </Grid>
     </ThemeProvider>
   );
---
+++ src/components/QuoteLeft.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import 'typeface-roboto-condensed';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Avatar from '@material-ui/core/Avatar';
+
+const useStyles = makeStyles({
+    root: {
+        flexGrow: 1,
+    },
+    avatar: {
+        margin: 'auto',
+        width: 300,
+        height: 300,
+        display: 'block',
+    },
+    paper: {
+        margin: 50,
+        maxWidth: '50%',
+        padding: 40,
+        marginBottom: 100,
+        textAlign: 'center',
+        backgroundColor: 'rgb(244,154,2, 0.75)',
+        color: '#FFFFFF',
+    },
+    author: {
+        textAlign: 'right',
+    },
+});
+
+export default function QuoteLeft(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+        <Paper className={classes.paper} >
+            <Grid container justify="center" alignItems="center">
+                <Grid item xs>
+                    <Avatar src={props.avatar} className={classes.avatar} />
+                </Grid>
+                <Grid item xs>
+                    <Typography variant="h5" gutterBottom>
+                        <em>{props.msg}</em>
+                    </Typography>
+                    <Typography variant="h5" className={classes.author} gutterBottom>
+                        - {props.author}
+                    </Typography>
+                    <Typography variant="h6" className={classes.author} gutterBottom>
+                        {props.title}
+                    </Typography>
+                </Grid>
+            </Grid>
+        </Paper>
+    </div>
+  );
+}
