0e92daf6e6756540035a89cf4b34dbeba7e05ebc
Hayden Pilsner <haydenpilsner@gmail.com>
2019-09-17 19:38:00 -0500

    Add five paid sponsors to sponsor section

--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -3,21 +3,42 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import 'typeface-roboto-condensed';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import Avatar from '@material-ui/core/Avatar';
+
+import amFam from '../img/sponsors/american_family.png';
+import polco from '../img/sponsors/polco.png';
+import mongoDB from '../img/sponsors/mongodb.png';
+import perBlue from '../img/sponsors/perblue.png';
+import facebook from '../img/sponsors/facebook.png';
 
 const useStyles = makeStyles({
+    root: {
+        flexGrow: 1
+    },
     header: {
         color: 'rgb(244,154,2)',
         textAlign: 'center',
     },
-    paper: {
-        maxWidth: 800,
-        padding: '50px',
-        marginBottom: '100px',
-        textAlign: 'center',
-        backgroundColor: 'rgb(0,36,61, 0.75)',
-        color: '#FFFFFF',
+    bigLogo: {
+        backgroundColor: 'rgb(248,248,248, 0.9)',
+        marginBottom: '50px',
         margin: 'auto',
+        width: 450,
+        height: 450,
+    },
+    mediumLogo: {
+        backgroundColor: 'rgb(248,248,248, 0.9)',
+        marginBottom: '50px',
+        margin: 'auto',
+        width: 300,
+        height: 300,
+    },
+    smallLogo: {
+        backgroundColor: 'rgb(248,248,248, 0.9)',
+        marginBottom: '50px',
+        margin: 'auto',
+        width: 150,
+        height: 150,
     }
 });
 
@@ -25,20 +46,29 @@ export default function Sponsors() {
   const classes = useStyles();
 
   return (
-    <React.Fragment>
-        <Grid item xs={12}>
-            <Typography variant="h1" component="h2" gutterBottom className={classes.header}>
-                Our sponsors
-            </Typography>
-        </Grid>
-        <Paper className={classes.paper} >
+    <div className={classes.root}>
+        <Grid container ontainer>
             <Grid item xs={12}>
-                <Typography variant="h5" gutterBottom>
-                    Sponsors to be announced soon!
-                    Interested in sponsoring? Contact us at <a style={{color: 'rgb(244,154,2)'}} href="mailto:=sponsorship@madhacks.io">sponsorship@madhacks.io</a> 
+                <Typography variant="h1" component="h2" gutterBottom className={classes.header}>
+                    Our sponsors
                 </Typography>
             </Grid>
-        </Paper>
-    </React.Fragment>
+                <Grid item xs={12} sm={6}>
+                    <Avatar src={amFam} className={classes.bigLogo}/>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <Avatar src={polco} className={classes.bigLogo}/>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Avatar src={perBlue} className={classes.smallLogo}/>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Avatar src={mongoDB} className={classes.mediumLogo}/>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Avatar src={facebook} className={classes.smallLogo}/>
+                </Grid>
+        </Grid>
+    </div>
   );
 }
